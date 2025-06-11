44d7b1032c9aeec91d653c31ba7810bcdd4d5589
okuya-aoto <aoto_okuya@sanotec.co.jp>
2025-06-11 11:02:17 +0900

    メッセージ位置変更

--- myproj_intro/src/jp/co/f1/intro/sample/sample.java
+++ myproj_intro/src/jp/co/f1/intro/sample/sample.java
@@ -1,14 +1,12 @@
 package jp.co.f1.intro.sample;
 
 public class sample {
-	
-
-
 	public static void main(String[] args) {
-		
-		String name = new String("moko");  //正式
-		String name1 = "モコ"; //疑似プリミティブ
+
+		//正式
+		//疑似プリミティブ
+		String name = new String("moko");  
+		String name1 = "モコ"; 
 
 	}
-
 }
